8728525d9b236e375809b46ef788dbcc58c09b3d
ZorinIvan <ivanzorin.mail@gmail.com>
2016-04-12 23:35:38 +0300

    new data structs

--- hw1_mips/sim_core.c
+++ hw1_mips/sim_core.c
@@ -3,78 +3,126 @@
 
 #include "sim_api.h"
 
-/*control data struct*/
-typedef struct _CONTROL{
-	SIM_cmd_opcode opcode;
-	bool reg_wr; //write or not to reg
+
+typedef enum {ENABLE=0, DISABLE} STATE;
+
+/********************control**********************/
+
+typedef struct _CONTROL {
+	//bool reg_wr; //write or not to reg
 	bool reg_dst; //choose between rt & rd fot i-type command
 	bool branch; // 
-	//bool ALUSrc; 
-	//bool PCSrc;
 	bool mem_r;
 	bool mem_wr;
 	bool mem_toReg;
 } CONTROL;
 
-/*Registers*/
+/*function that transmits opcode to control signals*/
+void ctrl(SIM_cmd_opcode opcode, CONTROL* ctrlr) {
+
+	ctrlr->reg_dst = (opcode != CMD_NOP) && (opcode != CMD_ADD) && (opcode != CMD_SUB);
+	ctrlr->branch = (opcode == CMD_BREQ) || (opcode == CMD_BRNEQ);
+	ctrlr->mem_r = (opcode == CMD_LOAD);
+	ctrlr->mem_wr = (opcode == CMD_STORE);
+	ctrlr->mem_toReg = (opcode == CMD_STORE);
+}
+
+/*reset control*/
+int reset_control(CONTROL* cntr) {
+	if (cntr == NULL) return -1;
+	cntr->branch = 0;
+	cntr->mem_r = 0;
+	cntr->mem_toReg = 0;
+	cntr->mem_wr = 0;
+	cntr->reg_dst = 0;
+	return 0;
+}
+
+/**********************************************************/
+
+
+/************************Registers*************************/
 typedef struct _REG_FILE {
-	int32_t regFile[SIM_REGFILE_SIZE]; /// Values of each register in the register file
+	bool enable; //?????
+	int32_t regFile[SIM_REGFILE_SIZE]; // Values of each register in the register file
 	int32_t read_data1, read_data2;
 }REG_FILE;
 
+//////*****************************************************************************change functions
+/*Gets register adress and returns value of the register*/
+void read_register(REG_FILE* reg, int reg1, int reg2) {
+	reg->read_data1 = reg->regFile[reg1];
+	reg->read_data2 = reg->regFile[reg2];
+}
+
+void write_register(REG_FILE* reg, int write_register, int32_t write_date) {
+	if (reg->enable) reg->regFile[write_register] = write_date;
+}
+
+/**********************************************************/
 
 
-/*register between fetch and decode*/
-typedef struct _IF_DEC_REG {
-	/*input*/
-	int32_t inst_in;
-	int pc_plus4;
+/******************reset and copy command*******************/
+int reset_command(SIM_cmd* command) {
+	if (command == NULL) return -1;
+	command->dst = 0;
+	command->isSrc2Imm = false;
+	command->opcode=0;
+	command->src1 = 0;
+	command->src2 = 0;
+}
 
-	/*output*/
-	int rs_out, rt_out, rd_out;
-	SIM_cmd_opcode opcode_out;
-}IF_DEC_REG;
+void cpyCMD(SIM_cmd *src_cmd, SIM_cmd *tar_cmd) {
+	tar_cmd->opcode = src_cmd->opcode;
+	tar_cmd->src1 = src_cmd->src1;
+	tar_cmd->src2 = src_cmd->src2;
+	tar_cmd->isSrc2Imm = src_cmd->isSrc2Imm;
+	tar_cmd->dst = src_cmd->dst;
+}
+
+/*****************************************************/
 
 
-/*register between decode and execute*/
-typedef struct _DEC_EXE_REG {
-	/*input*/
-	int32_t regRS_in, regRT_in;
+typedef struct _CORE_STATE
+{
+	int32_t pc; /// Value of the current program counter (at instruction fetch stage)
+	int32_t regFile[SIM_REGFILE_SIZE]; /// Values of each register in the register file
+	struct
+	{
+		SIM_cmd cmd;      /// The processed command in each pipe stage
+		int32_t src1Val;  /// Actual value of src1 (considering forwarding mux, etc.)
+		int32_t src2Val;  /// Actual value of src2 (considering forwarding mux, etc.)
+		CONTROL control;  ///values of control
+	} pipeStageState[SIM_PIPELINE_DEPTH];
+} CORE_STATE;
 
-	/*output*/
-	int rs_in_out, rd_in_out, rt_in_out;
 
-	/*control*/
-	CONTROL contorl;
-}DEC_EXE_REG;
 
-typedef struct _EXE_MEM_REG {
-	int32_t ALU_RES_in;
-	int32_t ALU_RES_out;
-	int MUX_RT_RD_in;
-	int MUX_RT_RD_out;
-}EXE_MEM_REG;
+/****************global variables*******************/
+CORE_STATE current_mips_state;
 
-typedef struct _MEM_WB_REG {
-	int32_t ALU_RES_in;
-	int32_t ALU_RES_out;
-	int32_t MEM_in_out;
-	int MUX_RT_RD_in;
-	int MUX_RT_RD_out;
-}MEM_WB_REG;
 
-typedef struct _REGFILE {
-	int32_t regNum[SIM_REGFILE_SIZE];
-	int32_t rs;
-	int32_t rt;
-}REGFILE;
+/**************************************************/
+
+
 
 
 
 int SIM_CoreReset(void)
 {
+	current_mips_state.pc = 0;
 
-	return -1;
+	for (int i = 0; i < SIM_REGFILE_SIZE; i++) { //reset pipeline
+		current_mips_state.regFile[i] = 0;
+	}
+
+	for (int i = 0; i < SIM_PIPELINE_DEPTH; i++) { //reset state
+		reset_command(&current_mips_state.pipeStageState[i].cmd);
+		reset_control(&current_mips_state.pipeStageState[i].control);
+		current_mips_state.pipeStageState[i].src1Val = 0;
+		current_mips_state.pipeStageState[i].src2Val = 0;
+	}
+	return 0;
 }
 
 void SIM_CoreClkTick(void)
@@ -83,4 +131,17 @@ void SIM_CoreClkTick(void)
 
 void SIM_CoreGetState(SIM_coreState *curState)
 {
+	curState->pc = current_mips_state.pc;
+	for (int i = 0; i < SIM_REGFILE_SIZE; i++)
+	{
+		curState->regFile[i] = current_mips_state.regFile[i];
+	}
+
+	for (int i = 0; i < SIM_PIPELINE_DEPTH; i++)
+	{
+		curState->pipeStageState[i].src1Val = current_mips_state.pipeStageState[i].src1Val;
+		curState->pipeStageState[i].src1Val = current_mips_state.pipeStageState[i].src2Val;
+		cpyCMD(&current_mips_state.pipeStageState[i].cmd, &curState->pipeStageState[i].cmd);
+	}
+
 }
