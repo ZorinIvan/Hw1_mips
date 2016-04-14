c5861e1faa9045cfb14b02e097fa3d7686661da8
ZorinIvan <ivanzorin.mail@gmail.com>
2016-04-15 02:51:17 +0300

    added: hdu, fowarding unit, ex.

--- hw1_mips/sim_core.c
+++ hw1_mips/sim_core.c
@@ -4,9 +4,10 @@
 #include "sim_api.h"
 
 
-typedef enum { ENABLE = 0, DISABLE } STATE;
 
-/********************control**********************/
+
+
+/********************data structs*******************/
 
 typedef struct _CONTROL {
 	//bool reg_wr; //write or not to reg
@@ -18,7 +19,52 @@ typedef struct _CONTROL {
 	bool pcSrc;
 } CONTROL;
 
-/*function that transmits opcode to control signals*/
+typedef struct _PC {
+	bool control_pc;
+	uint32_t out;
+}PC;
+
+
+typedef struct _CORE_STATE
+{
+	PC pc; /// Value of the current program counter (at instruction fetch stage)
+	int32_t regFile[SIM_REGFILE_SIZE]; /// Values of each register in the register file
+	bool hdu_flag; //flag that shows if a prev command in fetch was lw
+	struct
+	{
+		SIM_cmd cmd;      /// The processed command in each pipe stage
+		int32_t src1Val;  /// Actual value of src1 (considering forwarding mux, etc.)
+		int32_t src2Val;  /// Actual value of src2 (considering forwarding mux, etc.)
+		CONTROL control;  ///values of control
+		int dst_value;    // value of returning parameter (example: result of add; destination reg for lw)
+	} pipeStageState[SIM_PIPELINE_DEPTH];
+} CORE_STATE;
+
+/****************************************************/
+
+
+/****************global variables*******************/
+CORE_STATE mips_state;
+
+
+/**************************************************/
+
+
+/***************Function declaration***************/
+
+
+void ctrl(SIM_cmd_opcode opcode, CONTROL* ctrlr); //function that transmits opcode to control signals
+int reset_control(CONTROL* cntr); //reset control
+void hdu(); //Hazard detection unit
+static void forwarding(); //Forwarding unit
+static bool branch(); //return true if there is a branch ( good for deciding which pc is going to be next!!!)
+int reset_command(SIM_cmd* command); //resets all commands of state
+void cpyCMD(SIM_cmd *src_cmd, SIM_cmd *tar_cmd); //copies commands of one state to another
+void shift_pipe(); //moves all commands in mips to the next state
+
+/**************************************************/
+
+
 void ctrl(SIM_cmd_opcode opcode, CONTROL* ctrlr) {
 
 	ctrlr->reg_dst = (opcode != CMD_NOP) && (opcode != CMD_ADD) && (opcode != CMD_SUB);
@@ -29,7 +75,7 @@ void ctrl(SIM_cmd_opcode opcode, CONTROL* ctrlr) {
 	ctrlr->pcSrc = (opcode == CMD_BREQ) || (opcode == CMD_BRNEQ) || (opcode == CMD_BR);
 }
 
-/*reset control*/
+
 int reset_control(CONTROL* cntr) {
 	if (cntr == NULL) return -1;
 	cntr->branch = 0;
@@ -41,40 +87,9 @@ int reset_control(CONTROL* cntr) {
 	return 0;
 }
 
-/**********************************************************/
 
 
-/************************Registers*************************/
-typedef struct _REG_FILE {
-	bool control_write; 
-	int32_t regFile[SIM_REGFILE_SIZE]; // Values of each register in the register file
-	int32_t read_data1, read_data2;
-}REG_FILE;
-
-//////*****************************************************************************change functions
-/*Gets register adress and returns value of the register*/
-void read_register(REG_FILE* reg, int reg1, int reg2) {
-	reg->read_data1 = reg->regFile[reg1];
-	reg->read_data2 = reg->regFile[reg2];
-	return;
-}
-
-void write_register(REG_FILE* reg, int write_register, int32_t write_date) {
-	if (reg->control_write) reg->regFile[write_register] = write_date;
-	return;
-}
-
-/**********************************************************/
-
-
-/**********************PC**********************************/
-
-typedef struct _PC {
-	bool control_pc;
-	uint32_t out;
-} PC;
-
-int writePC(PC* pc, int32_t newPC) { // ????? may be no need
+/*int writePC(PC* pc, int32_t newPC) { // ????? may be no need
 	if (pc == NULL) return -1;
 	if (pc->control_pc == true) {
 		pc->out = newPC;
@@ -85,75 +100,56 @@ int writePC(PC* pc, int32_t newPC) { // ????? may be no need
 		return 0; //contorl is disabled. don't do any change
 	}
 }
-
-/**********************************************************/
-
+*/
 
 
+void hdu() {
+	if (mips_state.pipeStageState[1].cmd.src1 == mips_state.pipeStageState[2].cmd.dst ||
+		(mips_state.pipeStageState[1].cmd.isSrc2Imm == false && 
+			mips_state.pipeStageState[1].cmd.src2 == mips_state.pipeStageState[2].cmd.dst)) { //nop is needed
 
-/**************Hazard detection unit**********************/
-
-
-
-
-/*********************************************************/
-
-/*******************Forwarding unit***********************/
-
-static void forwarding(CORE_STATE* cst) {
-	if (cst->pipeStageState[3].cmd.opcode != 0) {
-		if (cst->pipeStageState[2].cmd.src1 == cst->pipeStageState[3].cmd.dst) //rs_exe vs. rd_mem
-			cst->pipeStageState[2].src1Val = cst->regFile[cst->pipeStageState[3].cmd.dst];
-		if (cst->pipeStageState[2].cmd.src2 == cst->pipeStageState[3].cmd.dst) //rt_exe vs. rd_mem
-			cst->pipeStageState[2].src2Val = cst->regFile[cst->pipeStageState[3].cmd.dst];
+		//stall
+		mips_state.pc.out -= 4;
+		reset_control(&mips_state.pipeStageState[1].control);
 	}
-	if ((cst->pipeStageState[4].cmd.opcode != 0) && (cst->pipeStageState[3].cmd.dst != cst->pipeStageState[4].cmd.dst)) {
-		if (cst->pipeStageState[2].cmd.src1 == cst->pipeStageState[4].cmd.dst) //rs_exe vs. rd_wb
-			cst->pipeStageState[2].src1Val = cst->regFile[cst->pipeStageState[4].cmd.dst];
-		if (cst->pipeStageState[2].cmd.src2 == cst->pipeStageState[4].cmd.dst) //rt_exe vs. rd_wb
-			cst->pipeStageState[2].src2Val = cst->regFile[cst->pipeStageState[4].cmd.dst];
-	}
-}
-
-
-
-static bool brunch(CORE_STATE* cst) {
-	if (cst->pipeStageState[3].control.branch == true) {
-		if (cst->pipeStageState[3].cmd.opcode == CMD_BR)
-			return true;
-		if (cst->pipeStageState[3].cmd.opcode == CMD_BREQ)
-			return (cst->pipeStageState[3].src1Val - cst->pipeStageState[3].src1Val == 0);
-		if (cst->pipeStageState[3].cmd.opcode == CMD_BRNEQ)
-			return (cst->pipeStageState[3].src1Val - cst->pipeStageState[3].src1Val != 0);
-	}
-}
-
-
-
-
-typedef struct _CORE_STATE
-{
-	PC pc; /// Value of the current program counter (at instruction fetch stage)
-	REG_FILE regFile; /// Values of each register in the register file
-	bool hdu_flag; //flag that shows if a prev command in fetch was lw
-	struct
+		
+	else //nop not needed
 	{
-		SIM_cmd cmd;      /// The processed command in each pipe stage
-		int32_t src1Val;  /// Actual value of src1 (considering forwarding mux, etc.)
-		int32_t src2Val;  /// Actual value of src2 (considering forwarding mux, etc.)
-		CONTROL control;  ///values of control
-	} pipeStageState[SIM_PIPELINE_DEPTH];
-} CORE_STATE;
-
-/****************global variables*******************/
-CORE_STATE mips_state;
-
-
-/**************************************************/
+		return;
+	}
+}
 
 
 
-/******************additional functions for implimentation*******************/
+static void forwarding() {
+	if ((mips_state.pipeStageState[3].cmd.opcode == CMD_ADD) && (mips_state.pipeStageState[3].cmd.opcode == CMD_SUB) && (mips_state.pipeStageState[3].cmd.opcode == CMD_LOAD)) {
+		if (mips_state.pipeStageState[2].cmd.src1 == mips_state.pipeStageState[3].cmd.dst) //rs_exe vs. rd_mem
+			mips_state.pipeStageState[2].src1Val = mips_state.regFile[mips_state.pipeStageState[3].cmd.dst];
+		if ((mips_state.pipeStageState[2].cmd.src2 == mips_state.pipeStageState[3].cmd.dst) && (mips_state.pipeStageState[2].cmd.isSrc2Imm == false)) //rt_exe vs. rd_mem
+			mips_state.pipeStageState[2].src2Val = mips_state.regFile[mips_state.pipeStageState[3].cmd.dst];
+	}
+	if ((mips_state.pipeStageState[3].cmd.opcode == CMD_ADD) && (mips_state.pipeStageState[3].cmd.opcode == CMD_SUB) && (mips_state.pipeStageState[3].cmd.opcode == CMD_LOAD) && (mips_state.pipeStageState[3].cmd.dst != mips_state.pipeStageState[4].cmd.dst)) {
+		if (mips_state.pipeStageState[2].cmd.src1 == mips_state.pipeStageState[4].cmd.dst) //rs_exe vs. rd_wb
+			mips_state.pipeStageState[2].src1Val = mips_state.regFile[mips_state.pipeStageState[4].cmd.dst];
+		if (mips_state.pipeStageState[2].cmd.src2 == mips_state.pipeStageState[4].cmd.dst) //rt_exe vs. rd_wb
+			mips_state.pipeStageState[2].src2Val = mips_state.regFile[mips_state.pipeStageState[4].cmd.dst];
+	}
+}
+
+
+
+static bool branch() { //return true if there is a branch ( good for deciding which pc is going to be next!!!)
+	if (mips_state.pipeStageState[3].control.branch == true) {
+		if (mips_state.pipeStageState[3].cmd.opcode == CMD_BR)
+			return true;
+		if (mips_state.pipeStageState[3].cmd.opcode == CMD_BREQ)
+			return (mips_state.pipeStageState[3].src1Val - mips_state.pipeStageState[3].src1Val == 0);
+		if (mips_state.pipeStageState[3].cmd.opcode == CMD_BRNEQ)
+			return (mips_state.pipeStageState[3].src1Val - mips_state.pipeStageState[3].src1Val != 0);
+	}
+}
+
+
 int reset_command(SIM_cmd* command) {
 	if (command == NULL) return -1;
 	command->dst = 0;
@@ -177,7 +173,6 @@ void shift_pipe() {
 		mips_state.pipeStageState[i] = mips_state.pipeStageState[i - 1];
 	}
 }
-/*********************************************************/
 
 
 /***********Implimentation of API functions*************************/
@@ -188,11 +183,8 @@ int SIM_CoreReset(void)
 	mips_state.pc.control_pc = true;
 	mips_state.hdu_flag = false;
 
-	mips_state.regFile.read_data1 = 0;
-	mips_state.regFile.read_data2 = 0;
-	mips_state.regFile.control_write = false;
 	for (int i = 0; i < SIM_REGFILE_SIZE; i++) { //reset registers
-		mips_state.regFile.regFile[i] = 0;
+		mips_state.regFile[i] = 0;
 	}
 
 	for (int i = 0; i < SIM_PIPELINE_DEPTH; i++) { //reset pipeline
@@ -200,6 +192,7 @@ int SIM_CoreReset(void)
 		reset_control(&mips_state.pipeStageState[i].control);
 		mips_state.pipeStageState[i].src1Val = 0;
 		mips_state.pipeStageState[i].src2Val = 0;
+		mips_state.pipeStageState[i].dst_value = 0;
 	}
 	return 0;
 }
@@ -209,7 +202,7 @@ void SIM_CoreClkTick(void)
 	SIM_cmd fetch_command;
 
 
-	/*FETCH*/
+	/**********************FETCH***********************/
 
 	shift_pipe();
 	SIM_MemInstRead(mips_state.pc.out, &fetch_command);
@@ -224,43 +217,65 @@ void SIM_CoreClkTick(void)
 	mips_state.pc.control_pc = true;
 	mips_state.pc.out += 4; //pc+4
 
-	//decide which PC will be next
 
-	/*DECODE*/
+	/******************DECODE***********************/
 
 
-	if (mips_state.pipeStageState[1].cmd.opcode != 0) //not nope
+	if (mips_state.pipeStageState[1].cmd.opcode != 0) //not nop
 	{
-		//TODO check copy of from reg file 
-		//mips_state.pipeStageState[1].src1Val = mips_state.regFile[mips.pipeStageState[1].cmd.src1];
-		if (mips_state.pipeStageState[1].cmd.isSrc2Imm == false)
+		mips_state.pipeStageState[1].src1Val = mips_state.regFile[mips_state.pipeStageState[1].cmd.src1];
+		if (mips_state.pipeStageState[1].cmd.isSrc2Imm == false) //src2 is a reg index
 		{
-			read_register(&mips_state.regFile, mips_state.pipeStageState[1].cmd.src1, mips_state.pipeStageState[1].cmd.src2);
-			mips_state.pipeStageState[1].src1Val = mips_state.pipeStageState[1].cmd.src1;
-			mips_state.pipeStageState[1].src2Val = mips_state.pipeStageState[1].cmd.src2;
+			mips_state.pipeStageState[1].src2Val = mips_state.regFile[mips_state.pipeStageState[1].cmd.src2];
 		}
-		else
-		{
-			read_register(&mips_state.regFile, mips_state.pipeStageState[1].cmd.src1, mips_state.pipeStageState[1].cmd.src2);
+		else //src2 is immidiate value
 			mips_state.pipeStageState[1].src2Val = mips_state.pipeStageState[1].cmd.src2;
-		}
-
 
 	}
-	
-	if (mips_state.pipeStageState[1].cmd.opcode == CMD_LOAD) //got lw. need put flag for further command (hdu)
+
+
+	if (mips_state.hdu_flag == true) //a command in EXE was lw. Activate HDU
+	{
+		hdu();
+	}
+
+	if (mips_state.pipeStageState[1].cmd.opcode == CMD_LOAD) //got lw. need to put flag for further command (hdu)
 	{
 		mips_state.hdu_flag = true;
 	}
 	else
 		mips_state.hdu_flag = false;
 
-	if (mips_state.hdu_flag == true) //check if there is data dependecy of prev. lw
+	/******************************EX**************************/
+
+	switch (mips_state.pipeStageState[2].cmd.opcode)
 	{
-
+	case CMD_NOP:
+		break;
+	case CMD_ADD:
+		forwarding();
+		mips_state.pipeStageState[2].dst_value = mips_state.pipeStageState[2].src1Val + mips_state.pipeStageState[2].src2Val;
+		break;
+	case CMD_SUB:
+		forwarding();
+		mips_state.pipeStageState[2].dst_value = mips_state.pipeStageState[2].src1Val - mips_state.pipeStageState[2].src2Val;
+		break;
+	case CMD_LOAD:
+		forwarding();
+		mips_state.pipeStageState[2].dst_value = mips_state.pipeStageState[2].src1Val - mips_state.pipeStageState[2].src2Val;
+		break;
+	case CMD_STORE:
+		forwarding();
+		mips_state.pipeStageState[2].dst_value = mips_state.pipeStageState[2].src1Val - mips_state.pipeStageState[2].src2Val;
+		break;
+	case CMD_BR:
+		break;
+	case CMD_BREQ: //DO we need forward here?? how do we work with branch: what is rs, rt, label??
+		break;
+	case CMD_BRNEQ: //DO we need forward here??
+					
+		break;
 	}
-
-
 
 
 
@@ -271,7 +286,7 @@ void SIM_CoreGetState(SIM_coreState *curState)
 	curState->pc = mips_state.pc.out;
 	for (int i = 0; i < SIM_REGFILE_SIZE; i++)
 	{
-		curState->regFile[i] = mips_state.regFile.regFile[i];
+		curState->regFile[i] = mips_state.regFile[i];
 	}
 
 	for (int i = 0; i < SIM_PIPELINE_DEPTH; i++)
